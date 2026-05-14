0ff5ed8e0b85bc85934b8eebb84666593c86a538
maitriishahh <maitrishah142004@gmail.com>
2026-05-14 20:54:46 +0530

    Implement document upload APIs and file processing pipeline

--- backend/app/api/upload_routes.py
+++ backend/app/api/upload_routes.py
@@ -1,12 +1,45 @@
-from fastapi import APIRouter
+import os
+import uuid
+from fastapi import APIRouter, UploadFile, File, Depends, HTTPException
+
+from backend.app.auth.dependencies import get_current_user
+from backend.app.services.document_service import process_upload_document
 
 router = APIRouter(
     prefix = "/upload",
     tags=['Upload']
 )
 
-@router.get("/")
-async def upload_test():
+UPLOAD_DIR =  "backend/app/uploads"
+os.makedirs(UPLOAD_DIR,exist_ok=True)
+
+@router.get("pdf")
+async def upload_pdf(file:UploadFile=File(...),
+current_user: dict = Depends(get_current_user)):
+    if not file.filename.endswith(".pdf"):
+        raise HTTPException(
+            status_code=400,
+            detail='Only PDF files allowed'
+        )
+    user_id = str(current_user["_id"])
+
+    user_folder = os.path.join(UPLOAD_DIR, user_id)
+    os.makedirs(user_folder,exist_ok=True)
+
+    unique_filename = f"{uuid.uuid4()}.pdf"
+
+    file_path = os.path.join(user_folder, unique_filename)
+
+    with open(file_path,"wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    await process_upload_document(
+        file_path=file_path,
+        user_id=user_id,
+        filename=file.filename
+    )
+
     return{
-        "message":"Upload route working"
+        "message":"PDF uploaded succssfully."
     }
