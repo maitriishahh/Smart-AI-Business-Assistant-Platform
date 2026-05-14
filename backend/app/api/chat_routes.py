b63c672ce54baf60ff8ed022b27759da25ad96c9
maitriishahh <maitrishah142004@gmail.com>
2026-05-14 20:54:25 +0530

    Add chat routes and conversational assistant APIs

--- backend/app/api/chat_routes.py
+++ backend/app/api/chat_routes.py
@@ -1,12 +1,39 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix = "/chat",
-    tags=['Chat']
+from fastapi import (
+    APIRouter,
+    Depends
 )
 
-@router.get("/")
-async def chat_test():
-    return{
-        "message":"Chat route working"
+from pydantic import BaseModel
+
+from backend.app.auth.dependencies import (
+    get_current_user
+)
+
+from backend.app.services.chat_service import (
+    ask_question
+)
+
+router = APIRouter(
+    prefix="/chat",
+    tags=["Chat"]
+)
+
+
+class ChatRequest(BaseModel):
+    question: str
+
+
+@router.post("/query")
+async def chat_query(
+    payload: ChatRequest,
+    current_user: dict = Depends(get_current_user)
+):
+
+    answer = await ask_question(
+        question=payload.question,
+        user_id=str(current_user["_id"])
+    )
+
+    return {
+        "answer": answer
     }
